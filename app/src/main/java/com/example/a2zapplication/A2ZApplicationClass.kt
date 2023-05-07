c9c7aaa81b6a72caf30fea0d85d88a1a2501dc88
Suhail <suhailabbas23@gmail.com>
2023-05-07 07:56:57 +0530

    Implemented Dagger Hilt - Dependency Injection to the project

---
+++ app/src/main/java/com/example/a2zapplication/A2ZApplicationClass.kt
@@ -0,0 +1,8 @@
+package com.example.a2zapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class A2ZApplicationClass : Application() {
+}
---
+++ app/src/main/java/com/example/a2zapplication/di/A2ZModule.kt
@@ -0,0 +1,11 @@
+package com.example.a2zapplication.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object A2ZModule {
+
+}
